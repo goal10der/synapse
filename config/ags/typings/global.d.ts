b7981990b42f19dbcf1300df2da5ae72ee9c4728
goal10der <goal10der@gmail.com>
2026-02-21 15:11:48 -0500

    updated global.d.ts

--- config/ags/typings/global.d.ts
+++ config/ags/typings/global.d.ts
@@ -2,19 +2,44 @@ declare module "*.css" {
   const classes: { [key: string]: string };
   export default classes;
 }
-
 declare module "ags" {
   export const For: any;
   export const This: any;
   export const With: any;
   export const onCleanup: any;
+  export const App: any;
+  export const Astal: any;
+  export const Gtk: any;
+  export const Gdk: any;
 
-  // NEW: The state management function
-  export function createState<T>(initialValue: T): [
-    (transform?: (value: T) => any) => any, // The Getter / Binding
-    (newValue: T | ((prev: T) => T)) => void, // The Setter
+  export function createState<T>(
+    initialValue: T,
+  ): [
+    (transform?: (value: T) => any) => any,
+    (newValue: T | ((prev: T) => T)) => void,
   ];
 
+  const _default: any;
+  export default _default;
+}
+
+declare module "ags/gtk4/app" {
+  export const App: any;
+  const _default: any;
+  export default _default;
+}
+
+declare module "ags/time" {
+  export const createPoll: any;
+  export const timeout: any;
+  export const interval: any;
+  const _default: any;
+  export default _default;
+}
+
+declare module "ags/process" {
+  export const execAsync: any;
+  export const exec: any;
   const _default: any;
   export default _default;
 }
@@ -24,19 +49,17 @@ declare module "ags/*" {
   export = mod;
 }
 
-declare module "ags/time" {
-  export const createPoll: any;
-  const _default: any;
-  export default _default;
-}
-
-declare module "ags/process" {
-  export const execAsync: any;
-  const _default: any;
-  export default _default;
-}
-
 declare module "@girs/*" {
   const mod: any;
   export = mod;
 }
+
+declare global {
+  namespace JSX {
+    interface IntrinsicAttributes {
+      $?: (self: any) => void;
+      setup?: (self: any) => void;
+    }
+  }
+}
+export {};
